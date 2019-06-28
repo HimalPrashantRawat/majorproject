d393271fa45e3ed565406846229d4f26abfa924a
HimalPrashantRawat <51640060+HimalPrashantRawat@users.noreply.github.com>
2019-06-28 10:11:13 +0545

    Add files via upload

--- countfaces.py
+++ countfaces.py
@@ -4,7 +4,7 @@ import time
 
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+#eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
 count = 0
@@ -13,14 +13,17 @@ while 1:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     if len(faces) != 0:
-    	print ("Number of faces detected: " + str(faces.shape[0]))
-    cv2.imshow('img',img)
-    if ('Number of faces detected' == 1):
-    	break
+    	print("face Detected")
+    	#print ("Number of faces detected: " + str(faces.shape[0]))
+    #cv2.imshow('img',img)
+    #if ('Number of faces detected' == 2):
+    #	break
 
     
     cv2.imwrite('captured faces/frame%d.jpg' % count, img)
     count = count +1
+
+    
     
     k = cv2.waitKey(30) & 0xff
     if k == 27:
